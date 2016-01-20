185ea2288ca972b6ab7f36ebb5d3cc8e8d01dc01
shasdas <shaswata.das@cognizant.com>
2016-01-20 22:36:00 +0530

    jscs problem fixed

--- SPHostedMVVM-ModelProject/Gulpfile.js
+++ SPHostedMVVM-ModelProject/Gulpfile.js
@@ -4,19 +4,29 @@ var jshint = require("gulp-jshint");
 var jscs = require("gulp-jscs");
 var gulputil = require("gulp-util");
 var stylish = require("jshint-stylish");
+var fail = require("gulp-fail");
+var gulpIf = require("gulp-if");
+var jscsStylish = require('gulp-jscs-stylish');
 
-gulp.task("default", function () {
+gulp.task("default", function() {
     log('Analyzing JS files with JSCS and JSHint');
 
     return gulp.src([
         './Scripts/Services/**/*.js/',
         './Scripts/ViewModels/**/*.js/'
     ])
-    .pipe(jscs())
-    .pipe(jscs.reporter('fail'))
     .pipe(jshint('.jshintrc'))
+    .pipe(jscs())
+    .pipe(jscs.reporter())
+    .pipe(jscs.reporter('fail'))
+    //.on('error', function () { })
+    //.pipe(jscsStylish.combineWithHintResults())
+    //.pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('jshint-stylish', { verbose: true }))
     .pipe(jshint.reporter('fail'));
+    //.pipe(gulpIf(function (file) {
+    //    return (file.jscs.errorCount > 0);
+    //}, fail("JSCS exited with errors!", true)));
 });
 
 /////////////
